0bc645bee12ff0dbd597ab4c23c2f7cf7d909969
maxidelgado <Lolxd69->
2020-10-10 22:00:19 -0300

    redeploy

--- main_test.go
+++ main_test.go
@@ -5,9 +5,6 @@ import (
 	"testing"
 )
 
-func init() {
-}
-
 func Test_handle(t *testing.T) {
 	host = "listener.logz.io"
 	port = "8071"
@@ -21,6 +18,17 @@ func Test_handle(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
+		{
+			name: "success",
+			args: args{
+				event: events.CloudwatchLogsEvent{
+					AWSLogs: events.CloudwatchLogsRawData{
+						Data: "H4sIAAAAAAAAAK1TW0/bMBT+K5G1x4b4+O68FdEhpDEQ7fYwgiY3dkqktukSt4wh/vtOUjaxCxKMPcX5jn2+y7HvyCp0nVuE2e0mkJwcjWfjz6eT6XR8PCEj0tysQ4uw0BK0pcApSISXzeK4bbYbrGTupsuWbjX3Ltu0jd+WsUvdpk4XTerDrl/uD0xjG9wKTzDKaAY0ozq7fPNuPJtMZ1fGaKdL6UAKIzQEWyltrRBsHpTzwLFFt513ZVtvYt2s39bLGNqO5JfkF06k6dLY1osFqr4aaCe7sI79zjtSe2TnUjNtlAVLJZNCCKCGg2KSUw6CWVDaMCUF1yAoN8oA11QxgQpijVlFt0LboNCE5VRxruzoR4bYfjobX8ySi/Bli1tPfJ5IrZQvPUuDcTwVUunUUlelc2EN5WjZViL5iGbQVp485FGsyf3oT8EaGVGnpMoaxfBrgWlplRDSYHIWRSstOXCpQD8p2MBjwXcFWYZdWBYkL0i9rpqCjAoSu+G/H1UKNKV6BjqXkFP+aaivusWwod0bHbC29gNmfWlLW81Tx7lIBdNlapE7Nb4SvuIeNNX7JiFeN/sz52dousc2Ll4PSLaDn/epIPd/S4QBZ30eYLVkDIxVmDVOy1rOqTUMV1pZyhQXEsdknkhE4JRfkYj4PZFu06y78G+RtBhlDpQKXKPOuO1ZGaX4O2/87dDs/6Rhn5nG5P3RS6/zq9VpSp+p7mJyfvby91bEo23r4vDigII+UCpZdUU8rJfL4JNHRRzFUDkNq6a9Tab1t4AoM8npIYLua/JQ+NCFnlsN+Mm6jo+bGHlg9m36ZK7uvwOxudMpdAUAAA==",
+					},
+				},
+			},
+			wantErr: false,
+		},
 		{
 			name: "success",
 			args: args{
